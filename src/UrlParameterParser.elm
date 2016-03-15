77f6d0714bb0918bd8e61ba880af874171943b19
Austin Bingham <austin@sixty-north.com>
2016-03-15 13:15:02 +0100

    Upgraded to 0.16

--- src/UrlParameterParser.elm
+++ src/UrlParameterParser.elm
@@ -1,4 +1,4 @@
-module UrlParameterParser(ParseResult(..), parseSearchString) where
+module UrlParameterParser (ParseResult(..), parseSearchString) where
 
 {-| Parse URL parameters. To use this, you'll need to create an input port, pass the search string to Elm,
 and then parse them with this function, then that can populate your model.
@@ -16,7 +16,7 @@ port locationSearch : String
 Then parse the value of the port - this example discards errors:
 ```
 parameters : Dict String String
-parameters = 
+parameters =
   case (parseSearchString locationSearch) of
     Error _ -> Dict.empty
     UrlParams dict -> dict
@@ -36,36 +36,47 @@ init maybeID = ...
 # Return type
 @docs ParseResult
 
- -}
+-}
 
 import Dict exposing (Dict)
 import String
 import UrlParseUtil exposing (..)
 
+
 {-|
   If parsing is successful, you get a UrlParams containing a dictionary of keys to values.
   Otherwise, an error string.
   If there are no parameters, you'll get an error description.
- -}
-type ParseResult = 
-    Error String
+-}
+type ParseResult
+  = Error String
   | UrlParams (Dict String String)
+
 
 {-|
   Given a search string of the form "?key=value&key2=val2"
   parse these into a dictionary of key to value.
- -}
+-}
 parseSearchString : String -> ParseResult
 parseSearchString startsWithQuestionMarkThenParams =
   case (String.uncons startsWithQuestionMarkThenParams) of
-    Nothing -> Error "No URL params"
-    Just ('?', rest) -> parseParams rest
+    Nothing ->
+      Error "No URL params"
+
+    Just ( '?', rest ) ->
+      parseParams rest
+
+    Just ( _, rest ) ->
+      Error "Invalid URL params strings"
+
 
 parseParams : String -> ParseResult
 parseParams stringWithAmpersands =
   let
-    eachParam = (String.split "&" stringWithAmpersands)
-    eachPair  = List.map (splitAtFirst '=') eachParam
+    eachParam =
+      (String.split "&" stringWithAmpersands)
+
+    eachPair =
+      List.map (splitAtFirst '=') eachParam
   in
     UrlParams (Dict.fromList eachPair)
-
